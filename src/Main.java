930bc43e6d82dbf78d6cdd511f5742c1cae4bfaf
USER <rhs9827@naver.com>
2025-06-05 11:44:23 +0900

    6_5(3)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+//        String color = "노랑";
+//
+//        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : (color == "보라") ? "보라" : "초록";
+//
+//        System.out.println(choiceColor);
 
-        for(int i = 1; i < 21; i++) {
-
-            if (i % 2 != 0 || i % 3 != 0 ) {
-                continue;
-            }
-
-            System.out.println(i);
+        String color = "빨강";
+        if (color == "빨강") {
+            System.out.println("빨강");
+        }else if(color == "파랑"){
+            System.out.println("파랑");
+        }else if(color == "보라"){
+            System.out.println("보라");
+        }else {
+            System.out.println("초록");
         }
-
     }
 }
